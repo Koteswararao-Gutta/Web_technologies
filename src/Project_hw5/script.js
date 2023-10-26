7965af7a26bf32ef78f2465505cce4922f98ca3b
Koteswararao Gutta <92368522+Koteswararao-Gutta@users.noreply.github.com>
2023-10-25 17:25:49 -0700

    Update script.js

--- src/Project_hw5/script.js
+++ src/Project_hw5/script.js
@@ -1,1 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var dropdown = document.getElementsByClassName("dropdown")[0];
+    dropdown.addEventListener("click", function() {
+        dropdown.getElementsByClassName("dropdown-content")[0].classList.toggle("show");
+    });
+
+    window.addEventListener("click", function(event) {
+        if (!event.target.matches('.dropbtn')) {
+            var dropdowns = document.getElementsByClassName("dropdown-content");
+            for (var i = 0; i < dropdowns.length; i++) {
+                var openDropdown = dropdowns[i];
+                if (openDropdown.classList.contains('show')) {
+                    openDropdown.classList.remove('show');
+                }
+            }
+        }
+    });
+});
 
